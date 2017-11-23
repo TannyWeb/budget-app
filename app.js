b228ea6981cd1a0e57deb4085b3b43ed30e48c12
Tanvir Ali <tanvirali@Tanvirs-MacBook-Pro.local>
2017-11-23 00:03:34 +0000

    setting up modules in app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+
+// BUDGET CONTROLLER
+var budgetController = (function(){
+
+
+})();
+
+
+// UI CONTROLLER
+var UIController = (function(){
+
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+  document.querySelector('.add__btn').addEventListener('click', function(){
+
+    // 1. Get the field input data
+
+    //  2. Add the item to the budget controller
+
+    //  3. Add the item to the UI
+
+    //  4. Calculate budget
+    
+    //  5. Display budget on the UI
+
+
+  })
+
+})(budgetController, UIController);
